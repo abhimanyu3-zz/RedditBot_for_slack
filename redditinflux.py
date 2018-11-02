abd38a65ee5d477bb634423a919e8835c4618a73
Abhimanyu <abhimanyu.abhiashu@gmail.com>
2018-11-02 13:01:46 -0400

    Add files via upload

--- redditinflux.py
+++ redditinflux.py
@@ -7,14 +7,15 @@ import re
 from credentials import *
 import sys
 from influxdb import InfluxDBClient
-client = InfluxDBClient(host='localhost', port=8086, username='gjhgj', password='fgfhg', ssl=True, verify_ssl=True)
+client = InfluxDBClient(host='localhost', port=8086,
+                        username='gjhgj', password='fgfhg', ssl=True, verify_ssl=True)
 client.switch_database('esdata')
-reddit = praw.Reddit(client_id='', \
-                     client_secret='', \
-                     user_agent='', \
-                     username='', \
+reddit = praw.Reddit(client_id='',
+                     client_secret='',
+                     user_agent='',
+                     username='',
                      password='')
- 
+
 dff = pd.read_csv("reddit1.csv")
 for index, row in dff.iterrows():
     rdt = row['reditname']
@@ -22,14 +23,14 @@ for index, row in dff.iterrows():
     print(rdt)
     print(domain)
     subreddit = reddit.subreddit(rdt)
-    top_subreddit = subreddit.new(limit = 1000)
-    topics_dict = { "title":[],
-                "score":[],
-                "id":[], "url":[],
-                "comms_num": [],
-                "created": [],
-                "body":[],
-                "subreddit":[],}
+    top_subreddit = subreddit.new(limit=1000)
+    topics_dict = {"title": [],
+                   "score": [],
+                   "id": [], "url": [],
+                   "comms_num": [],
+                   "created": [],
+                   "body": [],
+                   "subreddit": [], }
     for submission in top_subreddit:
         topics_dict["title"].append(submission.title)
         topics_dict["score"].append(submission.score)
@@ -40,21 +41,24 @@ for index, row in dff.iterrows():
         topics_dict["body"].append(submission.selftext)
         topics_dict["subreddit"].append(subreddit)
     topics_data = pd.DataFrame(topics_dict)
+
     def get_date(created):
         return dt.datetime.fromtimestamp(created)
     _timestamp = topics_data["created"].apply(get_date)
-    topics_data = topics_data.assign(timestamp = _timestamp).sort_values(by=['timestamp'])
+    topics_data = topics_data.assign(
+        timestamp=_timestamp).sort_values(by=['timestamp'])
     created_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=15)
- 
+
     topics_data = topics_data[(topics_data['timestamp'] > created_time) & (
-            topics_data['timestamp'] < datetime.datetime.utcnow())]
-    my_list = ['Maintenance', 'Scheduled', 'downtime', 'Voice', 'Problem', 'Outage', 'Service', 'Interruption','microphone', 'Downtime', 'Patch']
+        topics_data['timestamp'] < datetime.datetime.utcnow())]
+    my_list = ['Maintenance', 'Scheduled', 'downtime', 'Voice', 'Problem',
+               'Outage', 'Service', 'Interruption', 'microphone', 'Downtime', 'Patch']
     ndata = topics_data[topics_data['title'].str.contains(
-            "|".join(my_list), regex=True, flags=re.IGNORECASE)].reset_index(drop=True)
- 
-     
+        "|".join(my_list), regex=True, flags=re.IGNORECASE)].reset_index(drop=True)
+
     if len(ndata['title']) > 0:
-        json_body = [{"measurement":"brushEvents","tags": {"client": ndata['subreddit'][0]},"fields": {"headlines":len(ndata.index),"comments":sum(ndata['comms_num']),"score":sum(ndata['score'])}}]
+        json_body = [{"measurement": "brushEvents", "tags": {"client": ndata['subreddit'][0]}, "fields": {
+            "headlines": len(ndata.index), "comments": sum(ndata['comms_num']), "score":sum(ndata['score'])}}]
         client.write_points(json_body)
     else:
-        print('hi')
+print('hi')
